ec7414b67d35e3a955b17f2feefe160f87b8caa5
Adam Mwaniki <adamndegwa@protonmail.com>
2024-05-08 13:11:46 +0300

    Makes basic functionality for signup and login

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,15 @@
+// Dashboard.js
+
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Welcome to the Dashboard!</p>
+      <p>Brian feel free to delete the contents of this component, this was just a placeholder</p>
+    </div>
+  );
+}
+
+export default Dashboard;
